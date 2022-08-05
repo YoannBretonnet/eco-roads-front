09c831ac7a1bff6864c2568272ff5010b79e235e
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-08-05 09:19:17 +0200

    fix: "intérêts" mispelling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -64,7 +64,7 @@ function Header() {
         </Tooltip>
         )}
         <Fab variant="extended" aria-label="add" sx={{ margin: 'auto', gap: '1vh', fontWeight: 'bold' }} onClick={(() => dispatch(openCloseCarModal()))}>
-          Véhicule | Localisation | Intérets
+          Véhicule | Localisation | Intérêts
           <BiSearch size="3.1vh" />
         </Fab>
         <Box component="nav" sx={{ display: 'flex', justifyContent: 'center', marginTop: '1vh' }}>
